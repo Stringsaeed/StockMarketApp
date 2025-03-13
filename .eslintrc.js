6553b692d833bac2695c4f72a31f91d49aca2c5d
Muhammed Ibrahim <stringsaeed@gmail.com>
2025-03-13 21:54:01 +0400

    chore: update ESLint configuration for improved code quality

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,50 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['import', 'perfectionist'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      settings: {
+        'import/parsers': {
+          '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+          typescript: {},
+        },
+      },
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: true,
+      },
+      rules: {
+        'import/order': [
+          'error',
+          {
+            groups: [
+              'builtin',
+              'external',
+              'internal',
+              'parent',
+              'sibling',
+              'index',
+            ],
+            alphabetize: {
+              caseInsensitive: true,
+              order: 'asc',
+            },
+            'newlines-between': 'always',
+          },
+        ],
+        'perfectionist/sort-objects': [
+          'error',
+          {
+            type: 'line-length',
+            order: 'desc',
+          },
+        ],
+      },
+    },
+  ],
 };
